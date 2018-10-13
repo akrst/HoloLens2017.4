7028e317dcb973f07bf2207eb47b32e155b28cdf
1080TI\akira <akirasato@psychic-vr-lab.com>
2018-10-14 01:23:51 +0900

    1. オブジェクトアニメーション433体でも問題なく動くことを確認  2. シェーダー版とテクスチャ使用版の両方の動作を確認  3. ビルドなどのスピードも特に問題ない

---
+++ Assets/ObjectsChange.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectsChange : MonoBehaviour {
+	public GameObject[] targetGameObjects;
+	int count = 0;
+	int previousCount;
+	public float span;
+	float delta = 0;
+	
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < targetGameObjects.Length; i++)
+		{
+			GameObject targetGameObject = targetGameObjects[i];
+			targetGameObject.SetActive(false);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.delta += Time.deltaTime;
+		if (this.delta > this.span)
+		{
+			if (count >= targetGameObjects.Length)
+			{
+				count = 0;
+			}
+			delta = 0;
+			
+			if (previousCount != null) {
+				GameObject previousTargetGameObject = targetGameObjects[previousCount];
+				previousTargetGameObject.SetActive(false);
+			}  
+			GameObject currenTargetGameObject= targetGameObjects[count];
+			currenTargetGameObject.SetActive(true);
+			previousCount = count;
+			count++;
+		}
+
+	}
+}
---
+++ Assets/Template.shader
@@ -0,0 +1,23 @@
+﻿Shader "ShaderSketches/Template"{
+    Properties{
+        _MainTex("MainTex", 2D) = "white"{}    
+    }
+    
+    CGINCLUDE
+    #include "UnityCG.cginc" 
+    
+    float4 frag(v2f_img i) : SV_Target{
+        // 座標をそのまま色として出力している
+        return float4(i.uv.x, i.uv.y, 0, 1);
+    } 
+    ENDCG
+    
+    SubShader{
+        Pass{
+            CGPROGRAM
+            #pragma vertex vert_img
+            #pragma fragment frag
+            ENDCG
+        }
+    }
+}
